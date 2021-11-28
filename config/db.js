3a1cacbd162a00be6231e0b0d24bd7257bab47ce
Michael <michael.cacciano@gmail.com>
2021-11-27 21:28:18 -0500

    add try catch to connectDB

--- config/db.js
+++ config/db.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  });
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
 
-  console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
 };
 
 module.exports = connectDB;
